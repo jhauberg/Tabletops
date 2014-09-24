fa697a2dc1c523f63792c473f2a2ab649ecb6f10
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2014-09-24 20:47:25 +0200

    Add token representation

---
+++ tabletop/component/TTTokenRepresentation.h
@@ -0,0 +1,15 @@
+//
+//  TTTokenRepresentation.h
+//  Tabletops
+//
+//  Created by Jacob Hauberg Hansen on 21/09/14.
+//  Copyright (c) 2014 Jacob Hauberg Hansen. All rights reserved.
+//
+
+#import "TTRepresentationComponent.h"
+
+@interface TTTokenRepresentation : TTRepresentationComponent <NSCoding, NSCopying>
+
+@property (strong) id image;
+
+@end
---
+++ tabletop/component/TTTokenRepresentation.m
@@ -0,0 +1,47 @@
+//
+//  TTTokenRepresentation.m
+//  Tabletops
+//
+//  Created by Jacob Hauberg Hansen on 21/09/14.
+//  Copyright (c) 2014 Jacob Hauberg Hansen. All rights reserved.
+//
+
+#import "TTTokenRepresentation.h"
+
+NSString* const kTTTokenRepresentationImageKey = @"image";
+
+@implementation TTTokenRepresentation
+
+- (id) initWithCoder: (NSCoder *) decoder {
+    if ((self = [super init])) {
+        _image = [decoder decodeObjectForKey: kTTTokenRepresentationImageKey];
+    }
+    
+    return self;
+}
+
+- (void) encodeWithCoder: (NSCoder *) encoder {
+    [encoder encodeObject: _image forKey: kTTTokenRepresentationImageKey];
+}
+
+- (id) copyWithZone: (NSZone *) zone {
+    TTTokenRepresentation *component = [[[self class] allocWithZone: zone] init];
+    
+    if (component) {
+        component.image = self.image;
+    }
+    
+    return component;
+}
+
+- (BOOL) isEqual: (id) object {
+    if ([super isEqual: object]) {
+        TTTokenRepresentation *otherTokenRepresentation = (TTTokenRepresentation *)object;
+        
+        return [self.image isEqual: otherTokenRepresentation.image];
+    }
+    
+    return NO;
+}
+
+@end
