41160065909192b46dc8e863ad97af066b859a58
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2014-12-20 22:30:21 +0100

    Add state implementation that allows for undoing/redoing state altering actions

---
+++ editor/state/TTEditorGameState.h
@@ -0,0 +1,45 @@
+//
+//  TTEditorGameState.h
+//  Tabletops
+//
+//  Created by Jacob Hauberg Hansen on 17/12/14.
+//  Copyright (c) 2014 Jacob Hauberg Hansen. All rights reserved.
+//
+
+#import "TTGameState.h"
+
+#import "TTAction.h"
+#import "TTEditorAction.h"
+
+/**
+ Provides do/undo/redo functionality for state altering actions.
+ */
+@interface TTEditorGameState : TTGameState
+
+/**
+ All executed actions including those that have been undone.
+ 
+ Note that all undone actions will be removed once a new action is executed.
+ */
+@property (nonatomic, readonly) NSArray *actions;
+/**
+ All executed actions except those that have been undone.
+ */
+@property (nonatomic, readonly) NSArray *executedActions;
+
+/**
+ Execute an undoable action. 
+
+ If the action has been previously undone, then this will redo the action and all undone actions prior to this one, starting with the oldest action first.
+
+ If the action is a new one, then this will clear all actions that have previously been undone.
+ */
+- (BOOL) executeAction: (TTEditorAction *) action;
+/**
+ Undo an executed action.
+ 
+ If this is not the most recent action, then all actions that were executed after this one will also be undone.
+ */
+- (BOOL) undoAction: (TTEditorAction *) action;
+
+@end
---
+++ editor/state/TTEditorGameState.m
@@ -0,0 +1,128 @@
+//
+//  TTEditorGameState.m
+//  Tabletops
+//
+//  Created by Jacob Hauberg Hansen on 17/12/14.
+//  Copyright (c) 2014 Jacob Hauberg Hansen. All rights reserved.
+//
+
+#import "TTEditorGameState.h"
+
+@implementation TTEditorGameState {
+ @private
+    NSMutableArray *_executedActions;
+    TTEditorAction *_mostRecentlyUndoneAction;
+}
+
+- (id) init {
+    if ((self = [super init])) {
+        _executedActions = [[NSMutableArray alloc] init];
+    }
+
+    return self;
+}
+
+- (BOOL) executeAction: (TTEditorAction *) action {
+    if ([_executedActions containsObject: action]) {
+        if (action.isExecuted) {
+            return NO;
+        }
+
+        if (_mostRecentlyUndoneAction) {
+            return [self redoAction:
+                    action];
+        }
+    }
+
+    // todo: move to after undoing?
+    if (![action execute]) {
+        return NO;
+    }
+
+    if (_mostRecentlyUndoneAction) {
+        // clear all undone actions that were executed after the most recently undone action
+        NSInteger indexOfAction = [_executedActions indexOfObject:
+                                   _mostRecentlyUndoneAction];
+        
+        if (indexOfAction != NSNotFound) {
+            [_executedActions removeObjectsInRange:
+             NSMakeRange(indexOfAction, _executedActions.count - indexOfAction)];
+            
+            _mostRecentlyUndoneAction = nil;
+        }
+    }
+    
+    [_executedActions addObject: action];
+
+    return YES;
+}
+
+- (BOOL) undoAction: (TTEditorAction *) action {
+    if (![_executedActions containsObject: action]) {
+        return NO;
+    }
+
+    NSUInteger i = _executedActions.count - 1;
+
+    TTEditorAction *previouslyDoneAction = nil;
+
+    // undo every action that happened after the specified action to undo
+    while (![(previouslyDoneAction = [_executedActions objectAtIndex: i--]) isEqual: action]) {
+        if (![self undoAction: previouslyDoneAction]) {
+            break;
+        }
+    }
+
+    if (![action undo]) {
+        return NO;
+    }
+
+    _mostRecentlyUndoneAction = action;
+
+    return YES;
+}
+
+- (BOOL) redoAction: (TTEditorAction *) action {
+    NSInteger i = [_executedActions indexOfObject:
+                   _mostRecentlyUndoneAction];
+
+    TTEditorAction *previouslyUndoneAction = nil;
+
+    // execute every action that were undone prior to the specified action to redo
+    while (![(previouslyUndoneAction = [_executedActions objectAtIndex: i++]) isEqual: action]) {
+        if (![previouslyUndoneAction execute]) {
+            break;
+        }
+
+        _mostRecentlyUndoneAction = previouslyUndoneAction;
+    }
+
+    if (![action execute]) {
+        return NO;
+    }
+
+    if ([_executedActions count] > i) {
+        _mostRecentlyUndoneAction = [_executedActions objectAtIndex: i];
+    } else {
+        _mostRecentlyUndoneAction = nil;
+    }
+
+    return YES;
+}
+
+- (NSArray *) actions {
+    return [NSArray arrayWithArray:
+            _executedActions];
+}
+
+- (NSArray *) executedActions {
+    if (_mostRecentlyUndoneAction) {
+        return [_executedActions subarrayWithRange:
+                NSMakeRange(0, [_executedActions indexOfObject:
+                                _mostRecentlyUndoneAction])];
+    }
+    
+    return self.actions;
+}
+
+@end
